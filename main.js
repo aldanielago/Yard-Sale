ce2877a54a7b7d56b61b69fa216115eb3f58f8e0
Laura Aponte <aldanielago@gmail.com>
2023-03-13 20:45:48 -0500

    Añadiendo menus en mobile y desktop

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 const menu_email = document.querySelector('.nav_right');
 const desktop_menu = document.querySelector('.desktop-menu');
 
+const menu_hambur = document.querySelector('#menu-hambur');
+const mobile_menu = document.querySelector('.mobile-menu');
+
 menu_email.addEventListener('click', showDesktopMenu);
+menu_hambur.addEventListener('click', showMenuHambur);
 
 function showDesktopMenu(){
-    desktop_menu.classList.toggle('inactive')
+    desktop_menu.classList.toggle('inactive');
+}
+
+function showMenuHambur(){
+    mobile_menu.classList.toggle('inactive');
 }
